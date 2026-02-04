ad5b081ce7e25f07fe2fea41711b77caca7d9f23
Sl0niQ <31eriflleh@gmail.com>
2026-02-04 19:34:07 +0700

    plan: file containing the development stage added;
    database: ddl for report and fraud reference added; names for fact tables changed

--- src/sql_scripts/ddl/database.sql
+++ src/sql_scripts/ddl/database.sql
@@ -104,8 +104,9 @@ SELECT
 	update_dt
 FROM bank.clients;
 
---passport_blacklist
-CREATE TABLE bank.dwh_dim_passport_blacklist_hist (
+--passport_blacklist [fact]
+--DROP TABLE IF EXISTS bank.dwh_dim_passport_blacklist_hist;
+CREATE TABLE bank.dwh_fact_passport_blacklist (
 	passport_num 	VARCHAR(128) NULL,
 	entry_dt 		DATE NULL,
 	effective_from	DATE DEFAULT now()::DATE,
@@ -116,18 +117,19 @@ CREATE TABLE bank.dwh_dim_passport_blacklist_hist (
 
 --terminals
 CREATE TABLE bank.dwh_dim_terminals_hist (
-	terminal_id		VARCHAR(128),
-	terminal_type	VARCHAR(128),
-	terminal_city	VARCHAR(128),
+	terminal_id			VARCHAR(128),
+	terminal_type		VARCHAR(128),
+	terminal_city		VARCHAR(128),
 	terminal_address	VARCHAR(128),
-	effective_from	DATE DEFAULT now()::DATE,
-	effective_to 	DATE,
-	deleted_flag 	BOOLEAN DEFAULT FALSE,
-	is_current		BOOLEAN DEFAULT TRUE
+	effective_from		DATE DEFAULT now()::DATE,
+	effective_to 		DATE,
+	deleted_flag 		BOOLEAN DEFAULT FALSE,
+	is_current			BOOLEAN DEFAULT TRUE
 );
 
---transactions
-CREATE TABLE bank.dwh_dim_transactions_hist (
+--transactions [fact]
+--DROP TABLE IF EXISTS bank.dwh_dim_transactions_hist;
+CREATE TABLE bank.dwh_fact_transactions (
 	trans_id		VARCHAR(128),
 	trans_date		DATE,
 	card_num 		VARCHAR(128) NULL,
@@ -139,3 +141,24 @@ CREATE TABLE bank.dwh_dim_transactions_hist (
 	is_current		BOOLEAN DEFAULT TRUE	
 );
 
+--fraud_types [reference]
+CREATE TABLE bank.dwh_ref_fraud_types (
+	event_type		CHAR(1),
+	event_describ	VARCHAR(256)
+);
+
+INSERT INTO bank.dwh_ref_fraud_types (event_type, event_describ)
+VALUES 	('A', 'Совершение операции при просроченном или заблокированном паспорте'),
+		('B', 'Совершение операции при недействующем договоре'),
+		('C', 'Совершение операций в разных городах в течение одного часа'),
+		('D', 'Попытка подбора суммы');
+
+--report
+CREATE TABLE bank.rep_fraud (
+	event_dt 		DATE,			--Время наступления события. Если событие наступило по результату нескольких действий – указывается время действия, по которому установлен факт мошенничества.
+	passport		VARCHAR(15),	--Номер паспорта клиента, совершившего мошенническую операцию.
+	fio 			VARCHAR(128),	--ФИО клиента, совершившего мошенническую операцию.
+	phone 			VARCHAR(20),	--Номер телефона клиента, совершившего мошенническую операцию.
+	event_type 		CHAR(1),		--Описание типа мошенничества.
+	report_dt 		DATE 			--Время построения отчета.
+);
